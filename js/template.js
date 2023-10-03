c8174d1616d07d53e05b4ca8e3f9bb608f1eaf47
RCD2020 <dobertravis@icloud.com>
2023-10-02 19:21:36 -0600

    added linkedin and github to navbar

--- js/template.js
+++ js/template.js
@@ -1,8 +1,38 @@
-const NAVBAR = `<ul class="nav">
-<li class="navItem">
-    <a href="index.html"><img class="profile navPic" src="images/pfp.jpeg" height="100px" width="100px"/></a>
-</li>
-<li class="navItem">
-    <a href="index.html" class="navLink">Home</a>
-</li>
-</ul>`;
+function _hrefDeep(dirDeep)
+{
+    let href = '';
+    for (var i = 0; i < dirDeep; i++)
+    {
+        href = '../';
+    }
+
+    return href;
+}
+
+
+function navBar(dirDeep) {
+    let text = '<ul class="nav">';
+    text += '       <li class="navItem">';
+    text += '           <a href="' + _hrefDeep(dirDeep) + 'index.html">';
+    text += '               <img class="profile navPic" src="' + _hrefDeep(dirDeep) + 'images/pfp.jpeg" height="70px" width="70px"/>';
+    text += '           </a>';
+    text += '       </li>';
+    text += '       <li class="navItem">';
+    text += '           <a href="' + _hrefDeep(dirDeep) + 'index.html" class="navLink">';
+    text += '               <i class="w3navIcon fa fa-home"></i>';
+    text += '           </a>';
+    text += '       </li>';
+    text += '       <li class="navItem">';
+    text += '           <a class="navLink" href="https://www.linkedin.com/in/robert-davis-8b7903218" target="_blank" rel="noopener noreferrer">';
+    text += '               <i class="navIcon devicon-linkedin-plain"></i>';
+    text += '           </a>';
+    text += '       </li>';
+    text += '       <li class="navItem">';
+    text += '           <a class="navLink" href="https://github.com/RCD2020" target="_blank" rel="noopener noreferrer">';
+    text += '               <i class="navIcon devicon-github-original"></i>';
+    text += '           </a>';
+    text += '       </li>';
+    text += '   </ul>';
+
+    return text;
+}
