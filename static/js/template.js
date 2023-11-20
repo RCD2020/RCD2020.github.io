71b31a77ef8f58b5fc084a69e8dbfaa7c1641a7a
RCD2020 <dobertravis@icloud.com>
2023-11-20 13:42:18 -0700

    updated index head

--- static/js/template.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    let menu = document.getElementById('sidecont');
-
-    if (menu.style.display === 'none' || menu.style.display === '') {
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = '';
-    }
-}
